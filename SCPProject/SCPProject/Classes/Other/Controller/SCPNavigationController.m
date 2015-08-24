62875da5104045395abeb479c64bbd3a28ad1dd1
HuiHan <41809855@qq.com>
2015-08-25 04:27:35 +0800

    导航控制器左侧按钮的修改

--- SCPProject/SCPProject/Classes/Other/Controller/SCPNavigationController.m
+++ SCPProject/SCPProject/Classes/Other/Controller/SCPNavigationController.m
@@ -23,6 +23,33 @@
     
     [navBar setTitleTextAttributes:attrs];
 }
+/**
+ * 拦截所有push近来的控制器
+ */
+- (void)pushViewController:(nonnull UIViewController *)viewController animated:(BOOL)animated
+{
+    if (self.childViewControllers.count > 0) {
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [button setImage:[UIImage imageNamed:@"back"] forState:UIControlStateNormal];
+        button.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+        [button sizeToFit];
+        [button addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+        
+        viewController.hidesBottomBarWhenPushed = YES;
+    }
+    
+    // 这句super的push要放在后面, 让viewController可以覆盖上面设置的leftBarButtonItem
+    [super pushViewController:viewController animated:animated];
+}
+
+// 监听返回按钮的点击
+- (void)back
+{
+
+    [self popViewControllerAnimated:YES];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
