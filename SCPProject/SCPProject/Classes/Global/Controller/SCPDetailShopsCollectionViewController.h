762fb5d91368dcaeb9d2cc76cfd43cd45dc98fbb
HuiHan <41809855@qq.com>
2015-09-06 16:26:26 +0800

    commit

---
+++ SCPProject/SCPProject/Classes/Global/Controller/SCPDetailShopsCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCPDetailShopsCollectionViewController.h
+//  SCPProject
+//
+//  Created by 韩辉 on 15/9/5.
+//  Copyright © 2015年 liupuyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCPDetailShopsCollectionViewController : UICollectionViewController
+
+@end
